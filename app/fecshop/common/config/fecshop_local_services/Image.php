f2cda48c35cb224a7cb23101441a612c9293bce1
w6688j <953372680@qq.com>
2018-05-15 12:36:59 +0800

    init

---
+++ app/fecshop/common/config/fecshop_local_services/Image.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ *
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+return [
+    'image' => [
+        'appbase'    => [
+            
+            'common' => [
+                'basedir'    => '@appimage/common',
+                'basedomain' => '//img.top1buy.com',
+            ],
+            
+            'appadmin' => [
+                'basedir'    => '@appimage/appadmin',
+                'basedomain' => '//img2.top1buy.com',
+            ],
+            'appfront' => [
+                'basedir'    => '@appimage/appfront',
+                'basedomain' => '//img3.top1buy.com',
+            ],
+            
+            'apphtml5' => [
+                'basedir'    => '@appimage/apphtml5',
+                'basedomain' => '//img4.top1buy.com',
+            ],
+            'appserver' => [
+                'basedir'    => '@appimage/appserver',
+                'basedomain' => '//img5.top1buy.com',
+            ],
+        ],
+    ],
+];
